29f541f08102ebb93e06e2eddf6c20cb520aecfe
Andrii_Yaremenko <Andrii.Yaremenko@epicgames.com>
2023-05-15 14:46:37 +0300

    done tasks

---
+++ task3/task-1&2/CONSTANTS.js
@@ -0,0 +1,12 @@
+const EVENTS = {
+    BEGIN: 'BEGIN',
+    END: 'END',
+    DATA: 'DATA'
+}
+
+const URL = 'https://jsonplaceholder.typicode.com/posts/1';
+
+module.exports = {
+    EVENTS,
+    URL
+}
